dfbf5f2ba21215f91fdb0d5ad5a39f563c531200
Marcelo Santos <mjdos.2014@gmail.com>
2023-12-02 11:42:50 -0300

    Ajustes

--- routes/web.php
+++ routes/web.php
@@ -11,64 +11,38 @@ use App\Http\Controllers\{
 };
 
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-Route::middleware('guest')->group(function () {
-
-    Route::get('login', [LoginController::class, 'index'])->name('login');
-    Route::post('login', [LoginController::class, 'store']);
-    Route::get('/loginCliente', [AuthController::class, 'index'])->name('login.cliente');
-    Route::post('/loginCliente', [AuthController::class, 'store']);
-
+Route::get('/', function () {
+    return view('index');
 });
 
+Route::get('login', [LoginController::class, 'index'])->name('login');
+Route::post('login', [LoginController::class, 'store']);
 
+Route::get('/tesouro_direto', [TesouroDiretoController::class, 'index'])->name('tesouro_direto.index');
+Route::get('/titulosLista', [TitulosController::class, 'index'])->name('titulos.lista.index');
+Route::get('/titulosDetalhes/{id}', [TitulosController::class, 'show'])->name('titulos.show');
+Route::get('/titulosDetalhes/{id}', [TitulosController::class, 'show'])->name('titulos.show');
+Route::get('/titulosComprar/{id}', [TitulosController::class, 'comprar'])->name('titulos.comprar');
+Route::any('/titulos/cancelar/{id}', [TitulosController::class, 'cancelar'])->name('titulos.cancelar');
+Route::any('/titulos/finalizar/{id}', [TitulosController::class, 'finalizar'])->name('titulos.finalizar');
 
-Route::get('/teste', function () {
-    return view('painel.vendas.index');
-});
+Route::get('/market-place', [MarketPlaceController::class, 'index'])->name('mp.index');
+Route::get('/exchange', [MarketPlaceController::class, 'exchange'])->name('exchange.index');
 
-Route::get('/site', function () {
-    return view('site.index');
-});
-
+///Rotas Autenticadas
 Route::middleware('auth')->group(function () {
 
     Route::get('/home', [HomeController::class, 'index'])->name('home.index');
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-    // criação de títulos 
+    // Criação dos Títulos 
     Route::get('/CriarTitulos', [TitulosController::class, 'indexCriar'])->name('criarTitulos.index');
     Route::get('/CriarTitulo', [TitulosController::class, 'createIndex'])->name('createTitulos.index');
     Route::post('/CriarTitulo', [TitulosController::class, 'store'])->name('criarTitulos.store');
     Route::get('/Titulo/edit/{id}', [TitulosController::class, 'editIndex'])->name('editTitulo.edit');
     Route::post('/Titulo/update', [TitulosController::class, 'update'])->name('updateTitulo.update');
-
-
     Route::get('/EmitirTitulos', [TitulosController::class, 'indexEmitir'])->name('emitirTitulos.index');
     Route::get('/EmitirTitulo', [TitulosController::class, 'createEmitir'])->name('emitirTitulo.index');
     Route::post('/EmitirTitulo', [TitulosController::class, 'storeEmitir'])->name('emitirTitulos.store');
-    // Route::get('/Titulo/edit/{id}', [TitulosController::class, 'editIndex'])->name('editTitulo.edit');
-    // Route::post('/Titulo/update', [TitulosController::class, 'update'])->name('updateTitulo.update');
 
 });
-
-
-Route::get('/tesouro_direto', [TesouroDiretoController::class, 'index'])->name('tesouro_direto.index');
-Route::get('/titulosLista', [TitulosController::class, 'index'])->name('titulos.index');
-Route::get('/titulosDetalhes/{id}', [TitulosController::class, 'show'])->name('titulos.show');
-
-Route::get('/', [TitulosController::class, 'index'])->name('titulos.index');
-Route::get('/titulosDetalhes/{id}', [TitulosController::class, 'show'])->name('titulos.show');
-Route::get('/titulosLista', [TitulosController::class, 'index'])->name('titulos.index');
-Route::get('/titulosComprar/{id}', [TitulosController::class, 'comprar'])->name('titulos.comprar');
-Route::any('/titulos/cancelar/{id}', [TitulosController::class, 'cancelar'])->name('titulos.cancelar');
-Route::any('/titulos/finalizar/{id}', [TitulosController::class, 'finalizar'])->name('titulos.finalizar');
-//Route::get('/tesouro_direto', [TesouroDiretoController::class, 'index'])->name('tesouro_direto.index');
-
-Route::get('/mp', [MarketPlaceController::class, 'index'])->name('mp.index');
-Route::get('/exchange', [MarketPlaceController::class, 'exchange'])->name('exchange.index');
-
-
-Route::get('/rotaTeste', [TitulosController::class, 'teste'])->name('teste.meu');
